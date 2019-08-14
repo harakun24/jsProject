438b30491f09c12a3e9aff3f886e29145977ea32
123 <31>
2019-08-14 15:25:48 +0700

    faefaef

--- app/app.js
+++ app/app.js
@@ -1,23 +1,27 @@
 using("app/style/style.css");
-let part = createUi("div");
-let txtCode = createUi("textarea");
-let cssCode = createUi("textarea");
-let rnd = createUi("button");
-let wrapper = createUi("div");
-let wrapperRunner = createUi("div");
+let part = createUI("div");
+let txtCode = createUI("textarea");
+let cssCode = createUI("textarea");
+let jsCode = createUI("textarea");
+let rnd = createUI("button");
+let wrapper = createUI("div");
+let wrapperRunner = createUI("div");
 app.ready(function() {
   setTimeout(function() {
     txtCode.attr([{ type: "class", value: "txtbox" }]);
     txtCode.attr([{ type: "id", value: "txtbox" }]);
+    jsCode.attr([{ type: "class", value: "txtbox" }]);
+    jsCode.attr([{ type: "id", value: "jsbox" }]);
     cssCode.attr([
       { type: "class", value: "txtbox" },
       { type: "id", value: "cssbox" }
     ]);
-    render(wrapper);
-    render(wrapperRunner);
+    wrapper.render();
+    wrapperRunner.render();
+    wrapper.child(rnd);
     wrapper.child(txtCode);
     wrapper.child(cssCode);
-    wrapper.child(rnd);
+    wrapper.child(jsCode);
     wrapperRunner.child(part);
     part.attr([{ type: "id", value: "part" }]);
     wrapper.attr([{ type: "class", value: "wrapper" }]);
@@ -37,4 +41,7 @@ function run() {
   $("#part").css({ width: "100%", height: "100%" });
   part.innerHTML = $("#txtbox").val();
   $("style").append($("#cssbox").val());
+  let scr = createUI("script");
+  scr.append($("#jsbox").val());
+  scr.render();
 }
